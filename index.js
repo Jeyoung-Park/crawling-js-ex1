2676e1d581df40769de9d38e1fed7a668f00ff15
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-14 16:51:37 +0900

    feat: eloquence tistory blog 크롤링 주소 변경
    - 타이틀 크롤링

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const url = "https://eloquence-developers.tistory.com/category";
+
 const getHtml = async () => {
   try {
-    return await axios.get("https://www.yna.co.kr/sports/all");
+    return await axios.get(url);
   } catch (error) {
     console.error(error);
   }
@@ -16,26 +18,27 @@ getHtml()
     const $ = cheerio.load(html.data);
     // children: 인자로 html selector를 받아 cheerio 객체에서 선택된 html 문자열에서 해당하는 모든 태그들의 배열을 반환
     // li div.item-box01 div.news-con
-    const $bodyList = $('div.list-type038 ul.list').children('li');
+    const $bodyList = $("div.article_skin div.list_content");
 
-    // console.log(bodyList.html())
+    console.log($bodyList.length);
 
     // 인자로 콜백함수를 받아 태그들의 배열을 순회하면서 콜백함수를 실행
-    $bodyList.each(function(i, element){
-        console.log(`${i}번쨰 element`, $(this).find('div.item-box01').find('div.news-con').find('a.tit-wrap').find('strong.tit-news').text());
+    $bodyList.each(function (i, element) {
+      console.log(
+        `${i}번쨰 element`,
+        $(this).find("a.link_post strong.tit-post")
+      );
       ulList[i] = {
-        title: $(this).find('strong.tit-news').text(),
+        title: $(this).find("strong.tit_post").text(),
         // url: $(this).find("strong.news-tl a").attr("href"),
         // image_url: $(this).find("p.poto a img").attr("src"),
         // image_at: $(this).find("p.poto a img").attr("alt"),
-        // summary: $(this).find("p.lead").text().slice(0, -11),
+        // summary: $(this).find("p.l1ead").text().slice(0, -11),
         // date: $(this).find("span.p-time").text(),
       };
     });
 
-  
-
-    console.log('ulList, ', ulList);
+    console.log("ulList, ", ulList);
 
     const data = ulList.filter((n) => n.title);
     return data;
