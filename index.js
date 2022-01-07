ebd3943057868b97d7f5860a2dc7511c5ec930d2
Jeyoung-Park <jason9380@naver.com>
2022-01-07 19:24:15 +0900

    feat: 페이지네이션된 페이지로 스크랩할 수 있도록 수정

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const url = "https://search.naver.com/search.naver?sm=tab_hty.top&where=news&query=%EC%BD%94%EB%A1%9C%EB%82%98&oquery=https%3A%2F%2Fsearch.naver.com%2Fsearch.naver%3Fwhere%3Dnews&tqi=hOkFwdprvTossDpqtW0sssssssd-168386";
-
-const getHtml = async () => {
+const getHtml = async (paramUrl) => {
   try {
-    return await axios.get(url);
+    return await axios.get(paramUrl);
   } catch (error) {
     console.error(error);
   }
 };
 
-getHtml()
-  .then((html) => {
-    let ulList = [];
-    // 인자로 html 문자열을 받아 cheerio 객체로 반환
-    const $ = cheerio.load(html.data);
-    // children: 인자로 html selector를 받아 cheerio 객체에서 선택된 html 문자열에서 해당하는 모든 태그들의 배열을 반환
-    // li div.item-box01 div.news-con
-    const $bodyList = $("div.group_news ul.list_news").children("li.bx");
+const scrapSinglePageData = async (currentIndex) => {
+  const url = `https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EC%BD%94%EB%A1%9C%EB%82%98&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=28&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=${currentIndex}`;
 
-    console.log('bodyList, ', $bodyList.length);
+  return getHtml(url)
+    .then((html) => {
+      let ulList = [];
+      // 인자로 html 문자열을 받아 cheerio 객체로 반환
+      const $ = cheerio.load(html.data);
+      // children: 인자로 html selector를 받아 cheerio 객체에서 선택된 html 문자열에서 해당하는 모든 태그들의 배열을 반환
+      // li div.item-box01 div.news-con
+      const $bodyList = $("div.group_news ul.list_news").children("li.bx");
 
-    // 인자로 콜백함수를 받아 태그들의 배열을 순회하면서 콜백함수를 실행
-    $bodyList.each(function (i, element) {
-      ulList[i] = {
-        title: $(this).find("a.news_tit").text(),
-        url: $(this).find("a.news_tit").attr("href"),
-        summary: $(this).find("a.api_txt_lines").text(),
-        createdAt: $(this).find("span.info").text(),
-      };
+      // console.log("bodyList, ", $bodyList.length);
+
+      // 인자로 콜백함수를 받아 태그들의 배열을 순회하면서 콜백함수를 실행
+      $bodyList.each(function (i, element) {
+        ulList[i] = {
+          title: $(this).find("a.news_tit").text(),
+          url: $(this).find("a.news_tit").attr("href"),
+          summary: $(this).find("a.api_txt_lines").text(),
+          createdAt: $(this).find("span.info").text(),
+        };
+      });
+
+      const data = ulList.filter((n) => n.title);
+      return data;
+    })
+    .then((result) => {
+      console.log("result after getHtml, ", result.length);
+      return result;
     });
+};
 
-    console.log("ulList, ", ulList);
+const scrapTotalData = async (pageNumber) => {
+  let resultData = [];
+  for (let i = 0; i < pageNumber; i++) {
+    setTimeout(async () => {
+      const singlePageData = await scrapSinglePageData(1 + i * 10);
+      resultData=[...resultData, ...singlePageData]
+      console.log('resultData,', resultData.length)
+    }, 3000);
+  }
+  return resultData;
+};
 
-    const data = ulList.filter((n) => n.title);
-    return data;
-  })
-  .then((result) => {
-    console.log("result after getHtml, ", result);
-  });
+console.log(scrapTotalData(3).length);
